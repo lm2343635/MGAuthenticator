2596e03540e9b975df0324ba90c2248f27cf3723
Meng Li <lm2343635@126.com>
2018-07-20 14:26:26 +0900

    Add bometrics authenticate method.

--- Example/MGAuthenticator/ViewController.swift
+++ Example/MGAuthenticator/ViewController.swift
@@ -7,18 +7,28 @@
 //
 
 import UIKit
+import MGAuthenticator
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bometricsType: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        bometricsType.text = "\(MGAuthenticator.shared.biometricsType)"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func bometricsAuthenticate(_ sender: Any) {
+        MGAuthenticator.shared.authenticate { (success) in
+            DispatchQueue.main.async {
+                self.resultLabel.text = success ? "Success" : "Failed"
+            }
+        }
+        
     }
-
+    
 }
 
